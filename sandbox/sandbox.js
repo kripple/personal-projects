4f946ffea3e3a3dcad7e90d41ecf1009f8219509
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-07-26 22:22:29 -0400

    Move projects

---
+++ sandbox/sandbox.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	document.onkeypress=function(evt){
+		if( evt.keyCode == 13 )
+			alert("Can you hear me now?");
+	};
+
+	// $( document ).on( 'onkeypress', function ( evt ) {
+	//     if( evt.keyCode == 13 )
+	//     	alert("hi");
+	// } ); 
+
+} );
+
